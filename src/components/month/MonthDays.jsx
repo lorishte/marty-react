5d082f3c8cd21523568a3059ac74b6aef7ba0083
lorishte <lora.milcheva@outlook.com>
2024-03-01 18:40:31 +0200

    Week border bottom

--- src/components/month/MonthDays.jsx
+++ src/components/month/MonthDays.jsx
@@ -7,8 +7,12 @@ const MonthDays = (props) => {
   return (
     <div
       className={`${isOpen ? "h-fit pb-6" : "h-0"} grid grid-cols-week md:grid-cols-7 gap-4 px-4 overflow-hidden transition-[height] duration-700 ease-out`}>
-      {days.map(el =>
-        <CalendarCard key={el.date} data={el} defaultOpen={defaultOpen}/>
+      {days.map((el, index) => {
+          if (index % 7 === 0 && index !== 0) {
+            return <div className='col-span-7 border-b'/>
+          }
+          return <CalendarCard key={el.date} data={el} defaultOpen={defaultOpen}/>
+        }
       )}
     </div>
   );
